2ef421254395573bc1c9cc44c489bdb7fa8ec370
buyuanlee <buyuanlee@gmail.com>
2018-10-26 20:17:05 +0800

    首页组件及样式

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,2 +1,15 @@
-var Toast = require('../mod/toast.js').Toast;
-Toast('怎么肥四?我的小老弟？你可往后稍稍吧！')
+require('less/index.less');
+
+var NoteManager = require('mod/note-manager.js').NoteManager;
+var Event = require('mod/event.js');
+var WaterFall = require('mod/waterfall.js');
+
+NoteManager.load();
+
+$('.add-note').on('click', function() {
+    NoteManager.add();
+})
+
+Event.on('waterfall', function(){
+    WaterFall.init($('#content'));
+})
