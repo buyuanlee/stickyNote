b4acb326dddcc6c51e2491b7daa541862d5ddb35
buyuanlee <buyuanlee@gmail.com>
2018-10-28 23:26:50 +0800

    增查功能

--- model/note.js
+++ model/note.js
@@ -5,13 +5,30 @@ const sequelize = new Sequelize(undefined, undefined, undefined, {
 
     // 仅限 SQLite
     storage: '../database/database.sqlite',
-    });
+});
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
+//链接测试
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect to the database:', err);
+//     });
+
+
+const Note = sequelize.define('note', {
+    text: {
+        type: Sequelize.STRING
+    }
+});
+
+// force: true 如果表已经存在，将会丢弃表
+Note.sync().then(() => {
+    Note.create({text: 'hello world!'})
+}).then(() => {
+    Note.findAll().then(notes => {
+        console.log(notes)
     })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+});
