258e24264dd2a3fc272c57aff78b20194b7e264a
buyuanlee <buyuanlee@gmail.com>
2018-10-28 23:04:37 +0800

    数据库链接

--- model/note.js
+++ model/note.js
@@ -4,22 +4,14 @@ const sequelize = new Sequelize(undefined, undefined, undefined, {
     dialect: 'sqlite',
 
     // 仅限 SQLite
-    storage: 'path/to/database.sqlite',
+    storage: '../database/database.sqlite',
+    });
 
-    // 请参考 Querying - 查询 操作符 章节
-    operatorsAliases: false
-});
-
-const User = sequelize.define('user', {
-    username: Sequelize.STRING,
-    birthday: Sequelize.DATE
-});
-
-sequelize.sync()
-    .then(() => User.create({
-        username: 'janedoe',
-        birthday: new Date(1980, 6, 20)
-    }))
-    .then(jane => {
-        console.log(jane.toJSON());
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
     });
